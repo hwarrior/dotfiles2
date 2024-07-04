cb3b5db6f7f26dfb94ad0adb8fa8cfa6a4fee7c7
Attila <szok.reg@gmail.com>
2024-07-04 11:34:27 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat bashrc.additions >> ~/.bashrc
+
+cp ./.gitmessage ~
+cp ./.gitconfig ~
+
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh & plugins
+cp ./.zshrc ~
